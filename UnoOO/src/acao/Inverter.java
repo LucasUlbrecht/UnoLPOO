08bcfdce4b7b23f545d6f05685059b14a8900f94
Pepenette <111522963+Pepenette@users.noreply.github.com>
2022-10-04 00:14:13 -0300

    Update Inverter.java

--- UnoOO/src/acao/Inverter.java
+++ UnoOO/src/acao/Inverter.java
@@ -3,24 +3,19 @@ import base.Roda;
 /**
   * @author Pedro;
   * @version 1.0;
-  
-
-*/
+  */
 
 public class Inverter extends Acao {
-  public Inverter() {
-  }
+ /**
+  * @deprecated;
+  * Função para inverter o sentido do jogo;
+  * @param Roda representa objeto referente a classe Roda;
+  * recebe o sentido e inverte, retornando a jogada para o jogador anterior;
+  * pode ser cumulativo, invertendo-se varias vezes o sentido e passando a jogada
+  * para o jogador resultante das constantes trocas; 
+  */
 
   public void realizar(Roda roda) {
     roda.inverter();
-  /**
-    * @deprecated;
-    * Função para inverter o sentido do jogo;
-    * @param Roda representa objeto referente a classe Roda;
-    * recebe o sentido e inverte, retornando a jogada para o jogador anterior;
-    * pode ser cumulativo, invertendo-se varias vezes o sentido e passando a jogada
-    * para o jogador resultante das constantes trocas;
-    * @return 
-  */
   }
 }
