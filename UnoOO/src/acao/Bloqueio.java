adc4cacbae7b660a92f684618c06b89a13edc9e0
Lucas <l_ulbrecht@ufms.br>
2022-10-10 10:07:40 -0400

    Get para bloqueio

--- UnoOO/src/acao/Bloqueio.java
+++ UnoOO/src/acao/Bloqueio.java
@@ -10,12 +10,27 @@ import base.Roda;
 
 public class Bloqueio extends Acao {
   /**
+   * Pula a jogada de um jogador, usando a função pular da roda
+   * (roda.pular()), para o proximo jogador
+   * 
    * @param roda - é a roda usada no próprio jogo
    * @return
-   *         Pula a jogada de um jogador, usando a função pular da roda
-   *         (roda.pular()), para o proximo jogador
+   * 
    */
   public void realizar(Roda roda) {
     roda.pular();
+  }
+
+  public void realizar(Roda roda, int n) {
+    return;
+  }
+
+  /**
+   * função que retorna qual o tipo da ação
+   * 
+   * @return String bloqueio
+   */
+  public String getAcao() {
+    return "Bloqueio";
   }
 }
