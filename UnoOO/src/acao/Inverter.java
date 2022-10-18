a546b8ddbef18199159e589d86c737c1cd522c92
Lucas Ulbrecht <98244995+LucasUlbrecht@users.noreply.github.com>
2022-10-17 23:40:07 -0400

    Atualização de funções (comentários e getAcao)

--- UnoOO/src/acao/Inverter.java
+++ UnoOO/src/acao/Inverter.java
@@ -1,27 +1,38 @@
 package acao;
-
 import base.Roda;
-
+import base.Jogada;
 /**
- * @author Pedro;
- * @version 1.0;
- */
+  * @author Pedro;
+  * @version 2.0;
+  */
 
 public class Inverter extends Acao {
-  /**
-   * @deprecated;
-   * Função para inverter o sentido do jogo;
-   * 
-   * @param Roda representa objeto referente a classe Roda;
-   *             recebe o sentido e inverte, retornando a jogada para o jogador
-   *             anterior;
-   *             pode ser cumulativo, invertendo-se varias vezes o sentido e
-   *             passando a jogada
-   *             para o jogador resultante das constantes trocas;
-   * 
-   */
-
+ /**
+  * @deprecated;
+  * Função para inverter o sentido do jogo;
+  * @param Roda representa objeto referente a classe Roda;
+  * recebe o sentido e inverte, retornando a jogada para o jogador anterior;
+  */
   public void realizar(Roda roda) {
     roda.inverter();
   }
+  public void realizar(Roda roda, int n) {
+    System.out.println("Você está utilizando os parâmetros errados: Colocando um inteiro para uma ação de inverter");
+    return;
+  }
+  public void realizar (Jogada jogada) {
+    System.out.println("Você está utilizando os parâmetros errados: Colocando uma jogada para uma ação de inverter");
+    return;
+  }
+  public void realizar (Jogada jogada, Roda roda, int n) {
+    System.out.println("Você está utilizando os parâmetros errados: Colocando uma jogada e um inteiro para uma ação de inverter");
+    return;
+  }
+  /**
+  * Função para retornar qual a ação da carta
+  */
+  public String getAcao(){
+    return "Inverter";
+  }
+
 }
