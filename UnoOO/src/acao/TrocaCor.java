08b82eac00566fbd6797818a3a759c2b1fbdc74d
Vinicius-Wagner <114433234+Vinicius-Wagner@users.noreply.github.com>
2022-10-17 22:12:34 -0400

    add de coments

--- UnoOO/src/acao/TrocaCor.java
+++ UnoOO/src/acao/TrocaCor.java
@@ -10,12 +10,13 @@ public class TrocaCor extends Acao {
   /**
    * @deprecated 
    * @param jogada representa objeto do tipo referente a classe Jogada (precisamos conversar com os outros integrantes de carta,
-   * baralho e roda sobre como vai funcionar a dinâmica de mudar a cor)
-   * Na função "realizar", supostamente deve utilizar "jogada.mudarCor", de forma que altere para a cor desejada  
+   * baralho e roda sobre como vai funcionar a dinâmica de mudar a cor).
+   *
+   * Na função "realizar", supostamente deve utilizar "jogada.mudarCor", de forma que altere para a cor desejada
    */
 
   public void realizar(Jogada jogada) {
-    jogada.mudarCor(); 
+    jogada.mudarCor(); /** jogador atual irá escolher para qual cor deseja alterar */
   }
   public String getAcao(){
     return "TrocaCor"; /** para representar que a ação 'TrocaCor' foi realizada */
