4e462e716f297164fea54c0b726b40b80b9a0f64
RobertoFORTs <bobfortesneto@gmail.com>
2022-10-17 23:25:55 -0300

    Update Mais4.java

--- UnoOO/src/acao/Mais4.java
+++ UnoOO/src/acao/Mais4.java
@@ -2,8 +2,9 @@ package acao;
 import base.Roda;
 import base.Jogada;
 /**
- * @author RobertoFortes;
- * @version 2.0;
+ * @author RobertoFortes
+ * @since 17/10/2022
+ * @version 3.0
  */
 
 public class Mais4 extends Acao {
@@ -12,35 +13,56 @@ public class Mais4 extends Acao {
  * @param roda representa objeto do tipo referente a classe Roda que funciona de forma cíclica;
  * @param jogada representa objeto do tipo referente a classe Jogada;
  * @param n representa o inteiro que indica a quantidade de vezes que a carta Mais4 foi acumulada/repetida;
- * dentro da função realizar deve-se chamar, supostamente, "Jogada.mudarCor()";
- * Espera-se a junção de (jogador/jogada) e a implementação do seu código para execução mais precisa do código;
- * Agora, teremos que acumular os mais quatro e indicar o jogador que deverá comprar o respectivo número de cartas;
- * A primeira chamada de rodar.proximoJogador(retorna) significa a passagem de Acao para o proximo jogador;
- * ao final, chama-se a função "roda.pular()" para passar-se a vez para o jogador após o que comprou as cartas;
+ */
+
+/**
+ * Falta buscar conhecimento dos outros integrantes para implementar a troca de cor
+ * A primeira chamada de roda.proximoJogador() significa a passagem de Acao para o proximo jogador;
+ * Ao final, chama-se a função "roda.pular()" para passar-se a vez para o jogador após o que comprou as cartas;
  */
 
   public void realizar(Roda roda, Jogada jogada, int n) {
-    jogada.mudarCor(); /**  jogador 1 (ultimo a jogar a carta referente ao "Mais4") // espera-se mudança (jogada/jogador) */
+    jogada.mudarCor(); /** Precisamos conversar com outros integrantes para realizar mudanças nessa etapa */
     int x;
-    x = roda.proximoJogador();  /** jogador 1 -> jogador 2 // a função retorna inteiro que identifica o jogador */
+    x = roda.proximoJogador();  /** jogador 1 -> jogador 2 --- a função retorna inteiro que identifica o jogador */
     for(int cont = 0; cont < n*4 ; cont++){
-      roda.comprarCarta(roda.jogadores[x]); /** jogador 2 recebe cartas // acessa-se o jogador correspondente no vetor */
+      roda.comprarCarta(roda.jogadores[x]); /** jogador 2 recebe cartas --- acessa-se o jogador correspondente no vetor */
     }
     roda.pular(); /**  jogador 2 -> jogador 3 */
   }
+
   /**
   * foi adicionada a função getAcao tipo String;
   */
   public String getAcao(){
     return "Mais4";
   }
+
+  /**
+   * @param roda
+   */
   public void realizar (Roda roda) {
     return;
   }
+
+  /**
+   * @param jogada
+   */
   public void realizar (Jogada jogada) {
     return;
   }
+
+  /**
+   * @param roda
+   * @param n
+   */
   public void realizar(Roda roda, int n) {
     return;
   }
+
+  void realizar(Jogada jogada, Roda roda, int n) {
+    
+  }
+
+
 }
