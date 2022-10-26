d0ed15a66db6ae3295b0133d02c0f9532bd171b4
RobertoFORTs <bobfortesneto@gmail.com>
2022-10-26 15:50:50 -0300

    Update Mais4.java

--- UnoOO/src/acao/Mais4.java
+++ UnoOO/src/acao/Mais4.java
@@ -1,6 +1,5 @@
 package acao;
 import base.Roda;
-import base.Jogada;
 /**
  * @author RobertoFortes
  * @since 17/10/2022
@@ -11,68 +10,15 @@ public class Mais4 extends Acao {
 /**
  * @deprecated
  * @param roda representa objeto do tipo referente a classe Roda que funciona de forma cíclica
- * @param jogada representa objeto do tipo referente a classe Jogada
- * @param n variável usada para marcar o acúmulo total de cartas +4
  */
 
 
  /**
- * Resumo: O primeiro jogador muda a cor, o segundo compra as cartas e se passa a jogada para um terceiro jogador
+ * Resumo: O jogo ficou responsável de fazer a acumulação da quantidade de compras
  */
-  public void realizar(Roda roda, Jogada jogada, int n) {
-    jogada.mudarCor(); /** Precisamos conversar com outros integrantes para realizar mudanças nessa etapa (apenas exemplo, provavelmente está errado e será modificado)*/
-    int x;
-    x = roda.proximoJogador();  /** jogador 1 -> jogador 2 --- a função retorna inteiro que identifica o jogador */
-    for (int cont = 0; cont < n ; cont++) {
-      roda.comprarCarta(roda.jogadores[x]); /** jogador 2 recebe cartas --- acessa-se o jogador correspondente no vetor */
-    }
-    roda.pular(); /**  jogador 2 -> jogador 3 */
+  public void realizar(Roda roda) {
+    /**roda.mudarCor();*/ 
+    /** Precisamos conversar com outros integrantes para realizar mudanças nessa etapa de mudarCor*/
+    roda.comprar(4, roda.jogadorDaVez()); /**indica a compra de quatro cartas para o jogador correspondente*/
   }
-
-  /**
-  * foi adicionada a função getAcao tipo String
-  */
-  public String getAcao(){
-    return "Mais4";
-  }
-
-  /**
-  Todas as funções a partir deste comentário são para evitar bugs e alertar sobre uso errado da cartaAção
-  */
-
-    /**
-   * @param roda
-   */
-  public void realizar (Roda roda) {
-    System.out.println("Você está utilizando os parâmetros errados");
-    return;
-  }
-
-  /**
-   * @param jogada
-   */
-  public void realizar (Jogada jogada) {
-    System.out.println("Você está utilizando os parâmetros errados");
-    return;
-  }
-
-  /**
-   * @param roda
-   * @param n
-   */
-  public void realizar(Roda roda, int n) {
-    System.out.println("Você está utilizando os parâmetros errados");
-    return;
-  }
-  
-  /**
-   * @param jogada
-   * @param roda
-   * @param n
-   */
-  void realizar(Jogada jogada, Roda roda, int n) {
-    
-  }
-
-
 }
